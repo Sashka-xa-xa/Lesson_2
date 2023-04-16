d26964cad980e869d52d61d903e7a90c7ef3f7b7
Sashka-xa-xa <125003937+Sashka-xa-xa@users.noreply.github.com>
2023-04-16 23:32:00 +0900

    Add files via upload

---
+++ Lesson_2.java
@@ -0,0 +1,8 @@
+public class Lesson_2 {
+    public static void main(String[] args) {
+        double ticketCost = 20000;
+        int milesEarned = (int) (ticketCost / 20);
+        System.out.println("Заработано миль: " + milesEarned);
+
+    }
+}
